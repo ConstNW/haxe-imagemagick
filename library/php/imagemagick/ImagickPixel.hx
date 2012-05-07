a6292ddf8e2cb788c220fff78ba5c374e6cd58e3
Yaroslav@YHOST <Yaroslav@YHOST>
2012-05-07 19:18:00 +0400

    * minor ImagickPixel improvements

--- library/php/imagemagick/ImagickPixel.hx
+++ library/php/imagemagick/ImagickPixel.hx
@@ -16,4 +16,28 @@ import php.NativeArray;
 	public function setColor(color:String) : Bool;
 	public function setColorValue(color:Int, value:Float) : Bool;
 	public function setHSL(hue:Float, saturation:Float, luminosity:Float) : Bool;
+	
+	public function getAlpha() : Float
+	{
+		var c = getColor();
+		return c[untyped "a"] / 255.0;
+	}
+
+	public function getRed() : Float
+	{
+		var c = getColor();
+		return c[untyped "r"] / 255.0;
+	}
+
+	public function getGreen() : Float
+	{
+		var c = getColor();
+		return c[untyped "g"] / 255.0;
+	}
+
+	public function getBlue() : Float
+	{
+		var c = getColor();
+		return c[untyped "b"] / 255.0;
+	}
 }
