79ed29e4577793e4c15ce7490a14098fe973ed4a
Yaroslav <yar3333@gmail.com>
2013-10-03 15:29:22 +0400

    * recompilation
    --HG--
    rename : tests/support/pic1.jpg => tests/assets/pic1.jpg
    rename : tests/support/pic2.png => tests/assets/pic2.png
    rename : tests/support/pic4.png => tests/assets/pic4.png
    rename : tests/support/pic7-A.png => tests/assets/pic7-A.png
    rename : tests/support/pic7-B.png => tests/assets/pic7-B.png

--- build/make.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-call "c:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
-mingw32-make -f Makefile.win %*
--- tests/make.cmd
+++
@@ -1,4 +0,0 @@
-set NEKOPATH=%NEKOPATH%;..\..\library\ndll\Windows
-
-haxe -cp ../../library -cp . -main Main -neko Main.n
-neko Main.n
